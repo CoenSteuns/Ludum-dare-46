2d47e2bdce59df721319ae7871891921e0cf51da
Coen Steuns <c.steuns@gmail.com>
2020-04-18 21:59:14 +0200

    Card Hover Scripts

---
+++ Assets/Scripts/UI/CardHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private Vector2 originalsize;
+    private RectTransform rectTransform;
+
+    [SerializeField]
+    private float hoverSizeMultiplier = 1.2f;
+
+    private void Awake()
+    {
+        rectTransform = transform as RectTransform;
+        originalsize = rectTransform.sizeDelta;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rectTransform.sizeDelta = originalsize * hoverSizeMultiplier;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rectTransform.sizeDelta = originalsize;
+    }
+
+}
