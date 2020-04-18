170a5f43c9cc848a9bde3759950d9095c5fff6e4
jvdwijk <joeyvdwijk@gmail.com>
2020-04-18 16:44:01 +0200

    Text mesh pro + setup combat UI

--- Assets/Scripts/Combat System/Cards/Card.cs
+++ Assets/Scripts/Combat System/Cards/Card.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     [SerializeField]
+    private Sprite cardImage;
+
+    [SerializeField]
+    private string cardTitle;
+    [SerializeField]
     private AttackTypes attack;
     [SerializeField]
     private int damageAmount;
 
-    private void Use()
+    public int DamageAmount { get => damageAmount; }
+    public AttackTypes Attack { get => attack; }
+    public string CardTitle { get => cardTitle; }
+    public Sprite CardImage { get => cardImage; }
+
+    private void Use(Battle battle)
     {
 
     }
-
-
 }
---
+++ Assets/Scripts/Combat System/Cards/CardUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUI : MonoBehaviour
+{
+    [SerializeField]
+    private Card card;
+
+
+    private void Start()
+    {
+        if (!card) return;
+
+    }
+
+}
