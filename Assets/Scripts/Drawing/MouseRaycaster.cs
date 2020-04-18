958aedda834de2ba02fba1cc1a2e11b44d13a730
Coen Steuns <c.steuns@gmail.com>
2020-04-18 13:59:13 +0200

    line drawing

---
+++ Assets/Scripts/Drawing/MouseRaycaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MouseRaycaster : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private LayerMask mask = -1;
+
+    [SerializeField]
+    private float maxDistance = Mathf.Infinity;
+
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    public bool SendRay(out RaycastHit hit)
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        bool t = Physics.Raycast(ray, out hit, maxDistance, mask);
+        return t;
+    
+    }
+
+}
