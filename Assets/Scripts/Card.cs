76883a3ea76c94802f27c8c4bf0b7e3eb4e6eebc
jvdwijk <joeyvdwijk@gmail.com>
2020-04-18 14:14:06 +0200

    Attack types & card base

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private AttackTypes attack;
+    private int damageAmount;
+
+    private void Use()
+    {
+
+    }
+
+
+}
