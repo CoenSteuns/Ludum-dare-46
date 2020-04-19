da8dfdfb2243ae3a0bb600e900d1340b6d01709e
Coen Steuns <c.steuns@gmail.com>
2020-04-19 23:16:49 +0200

    music implemented

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip mainClip, combatClip;
+
+    [SerializeField]
+    private AudioSource source;
+
+    private void Awake()
+    {
+        SetCombat(false);
+    }
+
+    public void SetCombat(bool combat)
+    {
+        source.clip = combat ? combatClip : mainClip;
+        source.Play();
+    }
+
+}
