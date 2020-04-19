ff126a1349c314f8963d90e4b6b0e7ff65eb750a
Coen Steuns <c.steuns@gmail.com>
2020-04-20 00:41:19 +0200

    winbox

---
+++ Assets/Scripts/winnerwinnerchickendinner/WinBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinBox : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject winUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        winUI.SetActive(true);
+    }
+
+}
